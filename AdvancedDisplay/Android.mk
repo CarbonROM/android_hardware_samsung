a2a77195adeacf213499ab173caccdbd0b58d94a
Dominggoes Isakh <drjisakh@gmail.com>
2018-08-12 22:13:39 +0200

    Advanced Display: Set LOCAL_SDK_VERSION to current
    Change-Id: I47ed337672e739c15916e4871e96a8079b5554c1

--- AdvancedDisplay/Android.mk
+++ AdvancedDisplay/Android.mk
@@ -7,6 +7,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := AdvancedDisplay
 LOCAL_CERTIFICATE := platform
+LOCAL_SDK_VERSION := current
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
